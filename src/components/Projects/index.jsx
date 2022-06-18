fbdcd67fd7db179edffa10508a903021d5eb9e7e
Lucifer___13 <23053792+rajeshjaga@users.noreply.github.com>
2022-06-18 13:46:58 +0530

    aws hosting

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,24 +1,35 @@
 import React, { useEffect } from "react";
 import useFetch from "../../Api";
 import Card from "../card";
+
 const Projects = () => {
   let repoData = useFetch("https://api.github.com/users/rajeshjaga/repos");
-  if (repoData.data !== null && repoData.data !== undefined) {
-    let { data } = repoData.data;
+
+  if (repoData.loading === true) {
     return (
       <>
         <h1>check console</h1>
-        {data.map((item) => {
+        <h1>loading</h1>;
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div>Data is here</div>
+        {console.log(repoData.data)}
+      </>
+      /* {data.map((item) => {
           return (
             <div className="flex flex-wrap">
               <Card data={item} />
             </div>
           );
-        })}
-      </>
+        })} */
     );
-  } else {
-    return <h1>loading</h1>;
+    {
+      console.log(data);
+    }
   }
 };
+
 export default Projects;
